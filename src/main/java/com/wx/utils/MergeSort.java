0fd4c0acf502765cf8fa5bdaa0a7a715d26719a8
wuming-eb <wuming@eventbrite.com>
2017-04-03 00:08:26 -0700

    Merge sort using comparables and comparator. MergeSort tests

--- src/main/java/com/wx/utils/MergeSort.java
+++ src/main/java/com/wx/utils/MergeSort.java
@@ -1,20 +1,86 @@
 package com.wx.utils;
 
+import java.util.Comparator;
+
 public class MergeSort {
 
-    public static void recursiveSort(Comparable[] array) {
-
+    public static void sort(Comparable[] array) {
+        Comparable[] aux = new Comparable[array.length];
+        sort(array, aux, 0, array.length - 1);
     }
 
-    public static void bottomUpSort(Comparable[] array) {
-
-    }
-
-    private static void recursiveHelper() {
+    public static <T> void sort(T[] array, Comparator<? super T> comparator) {
+        T[] aux = array.clone();
+        sort(array, aux, 0, array.length - 1, comparator);
 
     }
 
     private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+        System.arraycopy(a, lo, aux, lo, hi - lo + 1);
 
+        int firstHalf = lo;
+        int secondHalf = mid + 1;
+        for (int i = lo; i <= hi; i++) {
+            if (firstHalf > mid) {
+                a[i] = aux[secondHalf++];
+            } else if (secondHalf > hi) {
+                a[i] = aux[firstHalf++];
+            } else if (less(aux[secondHalf], aux[firstHalf])) {
+                a[i] = aux[secondHalf++];
+            } else {
+                a[i] = aux[firstHalf++];
+            }
+        }
+    }
+
+    private static <T> void merge(T[] a, T[] aux, int lo, int mid, int hi, Comparator<? super T> comparator) {
+        System.arraycopy(a, lo, aux, lo, hi - lo + 1);
+
+        int firstHalf = lo;
+        int secondHalf = mid + 1;
+        for (int i = lo; i <= hi; i++) {
+            if (firstHalf > mid) {
+                a[i] = aux[secondHalf++];
+            } else if (secondHalf > hi) {
+                a[i] = aux[firstHalf++];
+            } else if (less(comparator, aux[secondHalf], aux[firstHalf])) {
+                a[i] = aux[secondHalf++];
+            } else {
+                a[i] = aux[firstHalf++];
+            }
+        }
+    }
+
+    private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+        if (lo < hi) {
+            int mid = lo + ((hi - lo) >> 1);
+            sort(a, aux, lo, mid);
+            sort(a, aux, mid + 1, hi);
+
+            // If it's already sorted do nothing.
+            if (less(a[mid + 1], a[mid])) {
+                merge(a, aux, lo, mid, hi);
+            }
+        }
+    }
+
+    private static <T> void sort(T[] a, T[] aux, int lo, int hi, Comparator<? super T> comparator) {
+        if (lo < hi) {
+            int mid = (lo + hi) >> 1;
+            sort(a, aux, lo, mid, comparator);
+            sort(a, aux, mid + 1, hi, comparator);
+
+            if (less(comparator, a[mid + 1], a[mid])) {
+                merge(a, aux, lo, mid, hi, comparator);
+            }
+        }
+    }
+
+    private static boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+
+    private static <T> boolean less(Comparator<? super T> comparator, T a, T b) {
+        return comparator.compare(a, b) < 0;
     }
 }
--- src/test/java/com/wx/coding/tests/SortTest.java
+++ src/test/java/com/wx/coding/tests/SortTest.java
@@ -1,4 +1,47 @@
 package com.wx.coding.tests;
 
+import com.wx.courses.algs4.Point;
+import com.wx.utils.MergeSort;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class SortTest {
+
+    @Test
+    public void testMergeSort() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new FileInputStream("src/test/resources/collinear/input10000.txt"));
+        int k = scanner.nextInt();
+        Point[] points = new Point[k];
+        Point[] copy = new Point[k];
+        for (int i = 0; i < k; i++) {
+            points[i] = new Point(scanner.nextInt(), scanner.nextInt());
+            copy[i] = points[i];
+        }
+
+        for (int i = 0; i < k; i++) {
+            MergeSort.sort(points);
+            Arrays.sort(copy);
+            compare(i, points, copy);
+
+            Comparator<Point> comparator = points[i].slopeOrder();
+            MergeSort.sort(points, comparator);
+            Arrays.sort(copy, comparator);
+            compare(i, points, copy);
+        }
+    }
+
+    private <T> void compare(int iteration, T[] a, T[] b) {
+        for (int i = 0; i < a.length; i++) {
+            AssertJUnit.assertTrue(
+                    "Iteration " + i + ", mismatch at index " + i + ": " + a[i] + ", " + b[i],
+                    ((Comparable) a[i]).compareTo(b[i]) == 0
+            );
+        }
+    }
 }
